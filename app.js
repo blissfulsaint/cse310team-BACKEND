6f3302ee71150cd533631a09e7f3308788c5bb4a
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-11-14 18:04:32 -0700

    whitelist created

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+
+var whitelist = [
+    '157.201.96.100', // Brandon's Laptop
+    'https://prayerselector.netlify.app' // Netlify app
+]
+
+var corsOptions = {
+    // origin: '*', // Testing only, comment out before pushing
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 const db = require('./models');
 db.mongoose
@@ -13,8 +30,10 @@ db.mongoose
         process.exit();
     })
 
-app
-    .use('/', require('./routes'));
+// Apply CORS middleware globally
+app.use(cors(corsOptions));
+
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
