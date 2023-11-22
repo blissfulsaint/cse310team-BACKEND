6468806d921127404fdb273e3c5364bb5635ae01
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-11-22 14:14:58 -0700

    tried cors fixing for render

--- app.js
+++ app.js
@@ -15,10 +15,17 @@ db.mongoose
 
 // Allow Netlify app to access the render app
 app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Origin', 'https://classroulette.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
 });
 
 app.use('/', require('./routes'));
